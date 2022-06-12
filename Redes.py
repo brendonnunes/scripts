f774173d57f036b15b33f937334c1cdbb4248e98
BRENDON NUNES <99628272+brendonnunes@users.noreply.github.com>
2022-06-12 12:24:47 -0300

    Update Redes.py

--- Redes.py
+++ Redes.py
@@ -1,15 +1,15 @@
-"""
-!/bin/bash
-Author: Brendon Guilherme Nunes dos Santos
-Support: brendondeeper@gmail.com
-
-"""
-import subprocess
+#!/bin/bash
+#Author: Brendon Guilherme Nunes dos Santos
+#Support: brendondeeper@gmail.com
+####################################################################
+#Boas-Vindas
+#import subprocess
 import os
 
 nome = input("Para nos conhecermos melhor, informe seu nome: ")
 print(f"Seja muito bem-vindo, {nome}")
 
+####################################################################
 #Instalação Maquina
 print("1 - GATEWAY\n2 - DNS\n3 - WEB")
 
@@ -18,13 +18,13 @@ print(maquina)
 
 if maquina == 1:
     print(f"Ok {nome}, será feita a requisição da instalação da maquina GATEWAY")
-    subprocess.call('sh'' curl -LO ')
+    os.system("sh installgtw.sh")
 if maquina == 2:
     print(f"Ok {nome}, será feita a requisição da instalação da maquina DNS")
-    #os.system(./installdns.sh)
+    os.system("sh installdns.sh")
 if maquina == 3:
     print(f"Ok {nome}, será feita a requisição da instalação da maquina WEB")
-    #os.system(./installweb.sh)
+    os.system("./installweb.sh")
 else:
     print("Nenhuma solicitação foi requisitada")
-    #os.system(exit)
+    os.system("exit")
